61732191bc85e814b66fd43d4f13d1d49ac96034
Fuad <fuadabdaz99@gmail.com>
2023-03-18 12:35:54 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  saturdayFun("hike"); // "This Saturday, I want to hike!"
+  saturdayFun("go to the beach"); // "This Saturday, I want to go to the beach!"
+  saturdayFun(); // "This Saturday, I want to roller-skate!"
+  
+//mondayWork function
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  mondayWork("work from home"); // "This Monday, I will work from home."
+  mondayWork("have a meeting"); // "This Monday, I will have a meeting."
+  mondayWork(); // "This Monday, I will go to the office."
+
+  function wrapAdjective(highlight = '*') {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
+  
